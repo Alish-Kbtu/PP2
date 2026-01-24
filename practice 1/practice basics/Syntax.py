038ec194c7f2407a921efc8e768c3b956fa4f0e7
Alisher <a_kinzhekov@kbtu.kz>
2026-01-24 19:29:27 +0500

    Add Syntax

---
+++ practice 1/practice basics/Syntax.py
@@ -0,0 +1,12 @@
+if 5 > 2:
+  print("Five is greater than two!")
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!")
+#This is a comment.
+print("Hello, World!")
+print("Hello World!")
+print("Have a good day.")
+print("Learning Python is fun!")
+print("Hello"); print("How are you?"); print("Bye bye!")
