49893c5eaf2b51833955e1bec70287c8e0c08a5b
Alisher <a_kinzhekov@kbtu.kz>
2026-01-24 20:01:32 +0500

    Add Strings

---
+++ practice 1/practice basics/Strings.py
@@ -0,0 +1,26 @@
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+b = "Hello, World!"
+print(b[2:5])
+
+b = "Hello, World!"
+print(b[:5])
+
+a = "Hello, World!"
+print(a.upper())
+
+a = "Hello, World!"
+print(a.split(",")) # returns ['Hello', ' World!']
+
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+
+price = 59
+txt = f"The price is {price} dollars"
+print(txt)
