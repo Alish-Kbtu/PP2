edc3578253f3d9165a3b0155456dc79692113a98
Alisher <a_kinzhekov@kbtu.kz>
2026-02-04 13:23:44 +0500

    Add for loop

--- practice 2/loops/for_loop.py
+++ practice 2/loops/for_loop.py
@@ -5,18 +5,6 @@ for x in fruits:
 for x in "banana":
   print(x)
 
-fruits = ["apple", "banana", "cherry"]
-for x in fruits:
-  print(x)
-  if x == "banana":
-    break
-  
-fruits = ["apple", "banana", "cherry"]
-for x in fruits:
-  if x == "banana":
-    break
-  print(x)
-
 for x in range(6):
   print(x)
 
