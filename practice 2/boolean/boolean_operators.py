a82b870e6ac52a94bd66e71da004cfd2b85e21b6
Alisher <a_kinzhekov@kbtu.kz>
2026-02-04 13:10:19 +0500

    Add bool comparison

---
+++ practice 2/boolean/boolean_operators.py
@@ -0,0 +1,12 @@
+bool(False)
+bool(None)
+bool(0)
+bool("")
+bool(())
+bool([])
+bool({})
+
+def myFunction() :
+  return True
+
+print(myFunction())
--- practice 2/if_else/if_else.py
+++ practice 2/if_else/if_else.py
@@ -1,17 +1,20 @@
-a = 200
-b = 33
-if b > a:
-  print("b is greater than a")
-elif a == b:
-  print("a and b are equal")
-else:
-  print("a is greater than b")
+number = 7
 
-a = 200
+if number % 2 == 0:
+  print("The number is even")
+else:
+  print("The number is odd")
+
+  a = 200
 b = 33
 if b > a:
   print("b is greater than a")
 else:
   print("b is not greater than a")
 
+  number = 7
 
+if number % 2 == 0:
+  print("The number is even")
+else:
+  print("The number is odd")
