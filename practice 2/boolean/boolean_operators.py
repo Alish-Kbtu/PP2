1973c7a23fa4f8e53c95599750a9a1968bc9f7ba
Alisher <a_kinzhekov@kbtu.kz>
2026-02-04 13:18:59 +0500

    Add bool operators

---
+++ practice 2/boolean/boolean_operators.py
@@ -0,0 +1,16 @@
+print(bool("Hello"))
+print(bool(15))
+
+x = "Hello"
+y = 15
+print(bool(x))
+print(bool(y))
+
+bool("abc")
+bool(123)
+bool(["apple", "cherry", "banana"])
+
+def myFunction() :
+  return True
+
+print(myFunction())
