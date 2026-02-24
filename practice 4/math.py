f079f3a239fdfeaeaa33fe52c6d7bccb5889fc33
Alisher <a_kinzhekov@kbtu.kz>
2026-02-24 18:31:34 +0500

    Add math

---
+++ practice 4/math.py
@@ -0,0 +1,17 @@
+import math
+a = int(input())
+print(math.radians(a))
+
+h = int(input())
+f = int(input())
+s = int(input())
+print((f + s) / 2 * h)
+
+n = int(input())
+l = int(input())
+ar = (n * l * l) / (4 * math.tan(math.pi/n))
+print(round(ar))
+
+d = int(input())
+v = int(input())
+print(d * v)
